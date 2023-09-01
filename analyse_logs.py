ff10590fb6ad614937f87bfc0483770532254e82
Jakub Vítovec <jakvitov@gmail.com>
2023-09-01 13:01:28 +0200

    Files moving added to a function

--- analyse_logs.py
+++ analyse_logs.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import matplotlib as plt
-import os
+import sys
 import shutil
 from datetime import date
 
+if len(sys.argv) < 3:
+    raise Exception("Not enough args to start.")
 
-log_directory = "./test_logs"
-dated_logs_directory = "./dated_logs"
+# Get logs directory and output logs directories from system args
+log_directory = sys.argv[1]
+dated_logs_directory = sys.argv[2]
 
 # Move the access and error logs to a separate folder
-shutil.move(log_directory + "/web_ui_access.log", dated_logs_directory + "/log_access_" + str(date.today()) + ".log")
-shutil.move(log_directory + "/web_ui_error.log", dated_logs_directory + "/log_error_" + str(date.today()) + ".log")
-
-
-
-
-
-#%%
+def moveFiles():
+    date_string = str(date.today())
+    print("Moving " + log_directory + "/web_ui_access.log" + " -> " + dated_logs_directory + "/log_access_" + date_string + ".log")
+    shutil.move(log_directory + "/web_ui_access.log", dated_logs_directory + "/log_access_" + date_string + ".log")
+    print("Moving " + log_directory + "/web_ui_error.log" + " -> " + dated_logs_directory + "/log_error_" + date_string + ".log")
+    shutil.move(log_directory + "/web_ui_error.log", dated_logs_directory + "/log_error_" + date_string + ".log")
