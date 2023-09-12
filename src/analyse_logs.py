08dd60be3f43775e93ee3767f26ff7aa46897251
jakvitov <jakvitov@gmail.com>
2023-09-12 21:21:11 +0200

    Logs analysis edited for the cron

--- src/analyse_logs.py
+++ src/analyse_logs.py
@@ -1,17 +1,12 @@
 import sys
 import datetime
-from scheduler import Scheduler
-from scheduler.trigger import Sunday
 
 from create_email_service import createAndSendEmail
 
 # Entrypoint to this whole app to be periodically launched
 
-
 if len(sys.argv) < 3:
     raise Exception("Not enough args to start.")
-
-
 
 def runEmailTask():
     # Get logs directory and output logs directories from system args
@@ -21,9 +16,5 @@ def runEmailTask():
     print("Started creating report at: " + datetime.datetime.now().ctime())
     createAndSendEmail(log_directory, dated_logs_directory)
 
-schedule = Scheduler()
-schedule.weekly(Sunday(datetime.time(hour=16)), runEmailTask)
-
 #We run the email task once to test, that it runs ok
 runEmailTask()
-print(schedule)
